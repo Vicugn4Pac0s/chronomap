03ea93f9cd0de76d140ee43e2071ac7e172b0b37
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2025-02-11 18:10:38 +0900

    TODOファイルを更新し、サインアウト状態のアイコンとログインボタンの追加を反映 Bodyコンポーネントにサインイン/サインアウト機能を実装

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import DrawerWrapper from "./DrawerWrapper";
 import PostForm from "./PostForm";
 import { Button } from "./ui/button";
@@ -13,22 +13,28 @@ import { Switch } from "./ui/switch";
 import { useState } from "react";
 
 const Body = () => {
-  const { data: session } = useSession();
+  const { data: session, } = useSession();
   const open = useDrawerStore(state => state.open);
   const [isPostMode, setIsPostMode] = useState(false);
 
   return (
     <div className="relative h-[100vh]">
-      {session?.user && (
         <>
           <div className="absolute top-0 right-0 p-2 z-50 w-full">
             <div className="flex justify-between align-middle">
-              <Avatar>
-                <AvatarImage src={session.user.image || ''} />
-              </Avatar>
+              {session?.user ? (
+                <div className="flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src={session.user.image || ''} />
+                  </Avatar>
+                  <Button onClick={()=>{ signOut(); }}>Sign Out</Button>
+                </div>
+              ): (
+                <Button onClick={()=>{ signIn('google'); }}>Sign in</Button>
+              )}
               <div className="flex items-center gap-2">
-                <Switch checked={isPostMode} onCheckedChange={(checked)=>{ setIsPostMode(checked) }} />
-                <Button onClick={()=>{open()}}>{ isPostMode ? 'POST': 'List' }</Button>
+                {session?.user && <Switch checked={isPostMode} onCheckedChange={(checked)=>{ setIsPostMode(checked) }} />}
+                <Button variant={'secondary'} onClick={()=>{open()}}>{ isPostMode ? 'POST': 'List' }</Button>
               </div>
             </div>
           </div>
@@ -37,7 +43,6 @@ const Body = () => {
             <PostForm />
           </DrawerWrapper>
         </>
-      )}
       <Clock className="absolute top-2 left-1/2 transform -translate-x-1/2 z-10" />
       <MapWrapper />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 import Body from "./components/Body";
 import Provider from "./provider";
-import { Button } from "./components/ui/button";
-import Link from "next/link";
 
 export default async function Home() {
-  const session = await auth();
-
-  if (session?.user) {
-    void api.post.get.prefetch();
-  }
 
   return (
     <HydrateClient>
@@ -19,11 +11,6 @@ export default async function Home() {
         <Provider>
           <Body />
         </Provider>
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
       </main>
     </HydrateClient>
   );
